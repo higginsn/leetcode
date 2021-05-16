3165da285ec5aeb9cb410e54aa9985aafa2382a4
Nick Higgins <higginsn@umich.edu>
2021-05-15 17:40:55 -0700

    TwoSums solution
    https://leetcode.com/problems/two-sum

--- src/main/java/com/higginsn/leetcode/TwoSums.java
+++ src/main/java/com/higginsn/leetcode/TwoSums.java
@@ -2,10 +2,17 @@ package com.higginsn.leetcode;
 
 public class TwoSums {
 
-    public static void main(String[] args) {
+    public int[] twoSums(int[] nums, int target) {
 
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{};
     }
-
-
 
 }
