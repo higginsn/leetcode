4d3463476b71e890f8cc3905e22ce0381a42478d
Nick Higgins <higginsn@umich.edu>
2021-06-05 11:06:35 -0700

    Optimized converting roman numerals to integers

--- src/main/java/com/higginsn/leetcode/algorithms/RomanToInteger.java
+++ src/main/java/com/higginsn/leetcode/algorithms/RomanToInteger.java
@@ -13,42 +13,30 @@ public class RomanToInteger {
     public int romanToInt(String s) {
         int result = 0;
         for (int i = 0; i < s.length() - 1; i++) {
-            RomanNumeral romanNumeral = RomanNumeral.valueOf(s.charAt(i));
-            RomanNumeral nextNumeral = RomanNumeral.valueOf(s.charAt(i + 1));
+            int currentValue = convertToInt(s.charAt(i));
+            int nextValue = convertToInt(s.charAt(i + 1));
 
-            if (romanNumeral.getValue() >= nextNumeral.getValue()) {
-                result += romanNumeral.getValue();
+            if (currentValue >= nextValue) {
+                result += currentValue;
             } else {
-                result -= romanNumeral.getValue();
+                result -= currentValue;
             }
         }
 
-        result += RomanNumeral.valueOf(s.charAt(s.length() - 1)).getValue();
-        return result;
+        return result + convertToInt(s.charAt(s.length() - 1));
     }
 
-    private enum RomanNumeral {
-        I(1),
-        V(5),
-        X(10),
-        L(50),
-        C(100),
-        D(500),
-        M(1000);
-
-        private final int value;
-
-        RomanNumeral(int value) {
-            this.value = value;
-        }
-
-        public static RomanNumeral valueOf(char c) {
-            return RomanNumeral.valueOf(String.valueOf(c));
-        }
-
-        public int getValue() {
-            return this.value;
-        }
+    private static int convertToInt(char c) {
+        return switch (c) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> 0;
+        };
     }
 
 }
