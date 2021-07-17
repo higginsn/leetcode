5ff1358a01c3be1dc1008658ffb83ab33688465f
Nick Higgins <higginsn@umich.edu>
2021-07-17 14:44:00 -0700

    Completed google interview process course

--- src/main/java/com/higginsn/leetcode/algorithms/google/interview/LicenseKeyFormatting.java
+++ src/main/java/com/higginsn/leetcode/algorithms/google/interview/LicenseKeyFormatting.java
@@ -1,13 +1,30 @@
 package com.higginsn.leetcode.algorithms.google.interview;
 
 /**
- * https://leetcode.com/explore/interview/card/google/67/sql-2/3045/
+ * https://leetcode.com/explore/interview/card/google/67/sql-2/472/
  */
-public class OddEvenJump {
+public class LicenseKeyFormatting {
 
-    public int oddEvenJumps(int[] arr) {
+    public String licenseKeyFormatting(String s, int k) {
+        int counter = k;
 
-        return 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (c == '-') {
+                continue;
+            }
+
+            if (counter == 0) {
+                stringBuilder.append('-');
+                counter = k;
+            }
+
+            stringBuilder.append(Character.toUpperCase(c));
+            counter--;
+        }
+
+        return stringBuilder.reverse().toString();
     }
 
 }
